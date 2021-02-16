a9c0ca150bac4c805cedafaf20489b907290da3a
Ivan Lychev <47522411+iLychevAD@users.noreply.github.com>
2021-02-16 18:00:01 +0300

    Create appcenter-post-clone.sh

---
+++ examples/hello_world/android/app/appcenter-post-clone.sh
@@ -0,0 +1,27 @@
+# https://www.rocksolidknowledge.com/articles/using-appcenter-for-flutter-projects
+
+#!/usr/bin/env bash
+#Place this script in project/android/app/
+
+cd ..
+
+# fail if any command fails
+set -e
+
+# debug log
+set -x
+
+cd ..
+
+git clone -b dev https://github.com/flutter/flutter.git
+
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
+
+flutter build apk --release
+
+#copy the APK where AppCenter will find it
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
